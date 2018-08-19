6e4a226aba5fd9fe019b39070ee78b75c1c32212
nkmrsts <29856021+nkmrsts@users.noreply.github.com>
2018-08-19 16:31:37 +0900

    promiseを使う

--- script.js
+++ script.js
@@ -1,63 +1,69 @@
-function getImage(text) {
-  const request = new XMLHttpRequest();
-  const api_key = '';
-  request.open(
-    'GET',
-    `https://api.flickr.com/services/rest/?method=flickr.photos.search
-&api_key=${api_key}&text=${text}&format=json&nojsoncallback=1`
-  );
-  request.addEventListener('load', event => {
-    if (event.target.status !== 200) {
-      console.log(`Error: ${event.target.status}`);
-      return;
-    }
-    console.log(event.target.status);
-    console.log(event.target.response);
-    const obj = JSON.parse(event.target.response);
-    const view = createView(obj);
+function main() {
+  const input = getSearchText();
+  if (input.length === 0) {
+    const view = `入力欄が空です。`;
     displayView(view);
+  }
+  getPhotos(input)
+    .then(photos => createView(photos))
+    .then(view => displayView(view))
+    .catch(error => {
+      console.error(`エラーが発生しました (${error})`);
+    });
+}
+function getPhotos(text) {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    const api_key = '';
+    request.open(
+      'GET',
+      `https://api.flickr.com/services/rest/?method=flickr.photos.search
+&api_key=${api_key}&text=${text}&per_page=16&format=json&nojsoncallback=1`
+    );
+    request.addEventListener('load', event => {
+      if (event.target.status !== 200) {
+        console.log(`Error: ${event.target.status}`);
+        reject(new Error(`${event.target.status}: ${event.target.statusText}`));
+      }
+      const photos = JSON.parse(event.target.response);
+      resolve(photos);
+    });
+    request.addEventListener('error', () => {
+      console.error('Network Error');
+      reject(new Error('ネットワークエラー'));
+    });
+    request.send();
   });
-  request.addEventListener('error', () => {
-    console.error('Network Error');
-  });
-  request.send();
+}
+function getSearchText() {
+  const value = document.getElementById('search').value;
+  return encodeURIComponent(value);
 }
 function createView(obj) {
-  const view = document.createElement('div');
+  const view = [];
   obj.photos.photo.forEach((v, i, a) => {
     const farm_id = v.farm;
-
-			
-const server_id = v.server;
-
-			
-const id = v.id;
-
-			
-const secret = v.secret;
-
-			
-const title = v.title;
+    const server_id = v.server;
+    const id = v.id;
+    const secret = v.secret;
+    const title = v.title;
     const imageURL = `https://farm${farm_id}.staticflickr.com/${server_id}/${id}_${secret}_m.jpg`;
-    const htmlString = `<a href="${imageURL}"><img src="${imageURL}" alt="${title}"></a>`;
-    view.insertAdjacentHTML('beforeend', htmlString);
+    const htmlString = `<div><a href="${imageURL}"><img src="${imageURL}" alt="${title}"></a></div>`;
+    view.push(htmlString);
   });
-  return view;
+  return view.join('');
 }
 function displayView(view) {
   const result = document.getElementById('result');
-  result.innerHTML = '';
-  result.appendChild(view);
+  result.innerHTML = view;
 }
 
-const searchForm = document.getElementById('search');
-const input = document.getElementById('input');
-searchForm.onsubmit = () => {
-  const inputText = input.value;
-  if (!inputText) return false;
-  getImage(inputText);
-  return false;
+const searchForm = document.getElementById('searchForm');
+searchForm.onsubmit = event => {
+  event.preventDefault();
+  main();
 };
+
 /*
 https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
 
